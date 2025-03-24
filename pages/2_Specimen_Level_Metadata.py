1b983078a32175d43b7e879da47623973159d3eb
Alfred Simkin <alfred.simkin@gmail.com>
2025-03-24 11:42:45 -0400

    first draft fixes

--- pages/2_Specimen_Level_Metadata.py
+++ pages/2_Specimen_Level_Metadata.py
@@ -16,35 +16,34 @@ class SpecimenMetadataPage:
         return st.file_uploader("Upload a TSV file", type=["csv", "tsv", "xlsx", "xls", "txt"])
 
     def field_mapping(self, df):
-        # Fuzzy field matching
-        field_mapping, unused_field_names = fuzzy_field_matching_page_section(
+        # Fuzzy field matching required arguments
+        mapped_fields, unused_field_names = fuzzy_field_matching_page_section(
             df, self.target_schema, self.alternate_schema_names)
 
         # Interactive field mapping
-        field_mapping = interactive_field_mapping_page_section(
-            field_mapping, df.columns.tolist())
-        return field_mapping, unused_field_names
+        mapped_fields = interactive_field_mapping_page_section(
+            mapped_fields, df.columns.tolist())
+        return mapped_fields, unused_field_names
 
-    def add_additional_fields(self, unused_field_names):
+    def add_additional_fields(self, df, unused_field_names):
         st.subheader("Add Additional Fields")
         selected_additional_fields = None
         if unused_field_names:
-            optional_additional_fields = st.toggle("Add additional fields")
-            if optional_additional_fields:
-                checkbox_states = {}
-                st.write("Select the extra columns you would like to include:")
-                for item in unused_field_names:
-                    checkbox_states[item] = st.checkbox(label=item)
-                selected_additional_fields = [
-                    key for key, value in checkbox_states.items() if value]
-                st.write("You selected:", selected_additional_fields)
-        return selected_additional_fields
+            # Fuzzy field matching optional arguments
+            mapped_fields, unused_field_names = fuzzy_field_matching_page_section(
+                df, schema_fields["specimen_level_metadata"]["optional"], 
+                schema_fields["specimen_level_metadata"]["optional_alternatives"])
 
-    def transform_and_save_data(self, df, field_mapping, selected_additional_fields):
+            #Interactive field mapping optional arguments
+            #mapped_fields = interactive_field_mapping_page_section(
+                #mapped_fields, df.columns.tolist())
+        return mapped_fields
+
+    def transform_and_save_data(self, df, mapped_fields, selected_additional_fields):
         st.subheader("Transform Data")
         if st.button("Transform Data"):
             transformed_df = transform_specimen_info(
-                df, field_mapping, selected_additional_fields)
+                df, mapped_fields, selected_additional_fields)
             st.session_state["specimen_info"] = transformed_df
             try:
                 st.success(
@@ -62,15 +61,15 @@ class SpecimenMetadataPage:
                 st.write("Uploaded File Preview:")
                 st.dataframe(df)
 
-            field_mapping, unused_field_names = self.field_mapping(df)
+            mapped_fields, unused_field_names = self.field_mapping(df)
 
             # Add additional fields
-            selected_additional_fields = self.add_additional_fields(
+            selected_additional_fields = self.add_additional_fields(df,
                 unused_field_names)
 
             # Transform and save data
             self.transform_and_save_data(
-                df, field_mapping, selected_additional_fields)
+                df, mapped_fields, selected_additional_fields)
 
 
 if __name__ == "__main__":
@@ -78,6 +77,6 @@ if __name__ == "__main__":
     st.subheader("Specimen Level Metadata Converter", divider="gray")
     schema_fields = load_schema()
     target_schema = schema_fields["specimen_level_metadata"]["required"]
-    alternate_schema_names = schema_fields["specimen_level_metadata"]["alternatives"]
+    alternate_schema_names = schema_fields["specimen_level_metadata"]["required_alternatives"]
     app = SpecimenMetadataPage(target_schema, alternate_schema_names)
     app.run()
